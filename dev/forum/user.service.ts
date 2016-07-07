3261c6ddd329cb8bcb393a89df6bf182f37d7b5f
Matthew Gardner <mgardner@kenzanmedia.com>
2016-07-07 17:01:30 -0400

    Removed unncessary http code

--- dev/forum/user.service.ts
+++ dev/forum/user.service.ts
@@ -2,12 +2,7 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class UserService {
-
-    getdata() {
-        return this._http.get('https://personal-site-85a8a.firebaseio.com/user/-KM5WtNX-y8SmvImmPg0.json')
-            .map(response => response.json());
-    }
-
+    
     createUser (data: any, username: string) {
         var fireBase = new Firebase('https://personal-site-85a8a.firebaseio.com');
         var dataRef = fireBase.child("user");
